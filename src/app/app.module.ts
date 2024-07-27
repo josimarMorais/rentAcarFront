1ea8c0fdc5684fba9c5cdff7007058de0ad5aa1c
Josimar Morais <josimar_morais@outlook.com>
2024-07-27 17:52:06 -0300

    Estruturação dos components/páginas
    Criação dos components:
    navbar, footer
    
    Das páginas:
    base-ui, user-list, user-create, user-update.
    
    Feita também a instalação do bootstrap e também inclusão do mesmo no arquivo angular.json na parte de styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { BaseUiComponent } from './pages/base-ui/base-ui.component';
+import { UserListComponent } from './pages/users/user-list/user-list.component';
+import { UserCreateComponent } from './pages/users/user-create/user-create.component';
+import { UserUpdateComponent } from './pages/users/user-update/user-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BaseUiComponent,
+    UserListComponent,
+    UserCreateComponent,
+    UserUpdateComponent
   ],
   imports: [
     BrowserModule,
